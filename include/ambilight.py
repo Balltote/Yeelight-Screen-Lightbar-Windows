47de0ca1751187e1f2b98d32bd867ce83c1a5e84
Balltote <104300074+Balltote@users.noreply.github.com>
2022-04-24 17:29:49 +0800

    Add files via upload

---
+++ include/ambilight.py
@@ -0,0 +1,63 @@
+import os
+from turtle import screensize
+from typing import List
+import cv2
+import numpy as np
+import time
+import pyautogui
+from mss import mss
+from yeelight.transitions import *
+from yeelight.flows import *
+from yeelight import *
+import tempfile
+
+fldr = tempfile.gettempdir()
+file = open(fldr+"/bulbIP.txt", "r")
+text = file.read()
+b = Bulb(text, effect="smooth", model="color")
+#b = Bulb("192.168.1.104", effect="smooth")
+b.turn_on(light_type=LightType.Ambient)
+b.set_brightness(100, light_type=LightType.Ambient)
+b.stop_flow(light_type=LightType.Ambient)
+screen_size = pyautogui.size()
+mon = {'top': int(screen_size[1]/2), 'left':int(screen_size[0]/2) , 'width': 1, 'height': 1}
+sct = mss()
+
+def b_set_rgb(rgb: List):
+	#b.set_rgb(int(rgb[0]), int(rgb[1]), int(rgb[2]), light_type=LightType.Ambient)
+		transitions = [
+    	RGBTransition(int(rgb[0]), int(rgb[1]), int(rgb[2]), duration=1000)
+	]
+
+		flow = Flow(
+    	count=0,  # Cycle forever.
+    	transitions=transitions
+	)
+		b.start_flow(flow, light_type=LightType.Ambient)
+		time.sleep(2)
+		b.stop_flow(light_type=LightType.Ambient)
+prev_rgb = np.array([0, 0, 0])
+
+while True:
+
+	sct_img = sct.grab(mon)
+	img = np.array(sct_img)
+	img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)[0][0]
+
+	if (prev_rgb == img_rgb).all():
+		continue
+
+	prev_rgb = img_rgb
+	b_set_rgb(img_rgb)
+
+	print(img_rgb)
+
+	cv2.waitKey(750)
+	# print(img_rgb)
+
+	# cv2.namedWindow('Screen Point', cv2.WINDOW_KEEPRATIO)
+	# cv2.imshow('Screen Point', img)
+
+	# if cv2.waitKey(1000) & 0xFF == ord('q'):
+	# 	cv2.destroyAllWindows()
+	# 	break
